51a9d0ae394cf392affd77c2042e58867d46d3f6
nina <nina.sohn@gmail.com>
2024-06-11 19:08:30 -0700

    Add authentication with Clerk

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
  - [ x ] Scaffold basic UI with mock data
  - [ x ] Clean up build process
  - [ x ] Set up a database (vercel postgres)
- - [ ] Attach database to UI
- - [ ] Add authentication (w/ clerk)
+ - [ x ] Attach database to UI
+ - [ x ] Add authentication (w/ clerk)
  - [ ] Add image upload
  - [ ] Error management (w/ Sentry)
  - [ ] Routing/image page (parallel route)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,6 @@
+import { ClerkProvider } from '@clerk/nextjs'
 import "~/styles/globals.css";
+import { TopNav } from './_components/topnav';
 
 import { GeistSans } from "geist/font/sans";
 
@@ -8,26 +10,23 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-function TopNav() {
-  return (
-    <nav className="flex w-full items-center justify-between p-4 text-xl font-semibold border-b">
-      <div>Gallery</div>
-      <div>Sign In</div>
-    </nav>
-  )
-}
-
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={`${GeistSans.variable}`}>
-      <body className="flex flex-col gap-4">
-        <TopNav />
-        {children}
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en" className={`${GeistSans.variable}`}>
+        <body className="flex flex-col gap-4">
+          <header>
+            <TopNav />
+          </header>
+          <main>
+            {children}
+          </main>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
